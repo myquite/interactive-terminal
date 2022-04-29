6a62c32bd092c42b43c58454507723f69de980f6
Mike White <myquite@gmail.com>
2022-04-29 08:57:13 -0500

    updated fileSystem.js

--- modules/filesystem.js
+++ modules/filesystem.js
@@ -1,15 +1,3 @@
-// function mockFileSystem() {
-//   return {
-//     root: "/Users/myquite",
-//     currentWorkingDirectory: "/Users/myquite/Sites/js-terminal",
-//     currentDirectories: ["styles", "scripts", "images"],
-//     currentFiles: ["index.html"],
-//     fileContents: {
-//       "index.html": "Hello World",
-//     },
-//   };
-// }
-
 function mockFileSystem() {
   return {
     root: "/Users/myquite",
