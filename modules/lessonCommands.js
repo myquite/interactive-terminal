a39d0286549ec8f38a0b68d04468aefeb77a4d63
Mike White <myquite@gmail.com>
2025-01-17 16:10:27 -0600

    Refactor lessonCommands for improved readability and update CSS gap for better layout

--- modules/lessonCommands.js
+++ modules/lessonCommands.js
@@ -1,15 +1,14 @@
 let lessonCommands = {
-    lesson: (arg) => {
-        let lessonNumber = arg.toString()
-        switch(lessonNumber) {
-            case "1":
-                let output = `<span class="cmd">Lesson ${arg}:</span> Find out where you are in the file system with the <em>'pwd'</em> command.`
-                return output
-            default:
-                console.log(`Sorry`);
-        }
+  lesson: (arg) => {
+    let lessonNumber = arg.toString();
+    switch (lessonNumber) {
+      case "1":
+        let output = `<span class="cmd">Lesson ${arg}:</span> Find out where you are in the file system with the <em>'pwd'</em> command.`;
+        return output;
+      default:
+        console.log(`Sorry`);
     }
-       
-  };
-  
-  export default lessonCommands;
+  },
+};
+
+export default lessonCommands;
