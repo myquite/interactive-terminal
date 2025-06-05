5eb7eed437843a9112103c39c40088f23b0a9114
Mike White <myquite@gmail.com>
2025-06-05 09:50:27 -0500

    Add lesson help command and update help section

--- modules/lessonCommands.js
+++ modules/lessonCommands.js
@@ -1,12 +1,22 @@
 let lessonCommands = {
-  lesson: (arg) => {
-    let lessonNumber = arg.toString();
+  lesson: (args) => {
+    // Normalize arguments coming from argv.args
+    const firstArg = Array.isArray(args) ? args[0] : args;
+
+    // Display usage information when called with --help or no args
+    if (!firstArg || firstArg === "--help" || firstArg === "-h") {
+      return (
+        "Use the <span class=\"cmd\">lesson &lt;number&gt;</span> command to " +
+        "start a guided exercise. Example: <span class=\"cmd\">lesson 1</span>."
+      );
+    }
+
+    const lessonNumber = firstArg.toString();
     switch (lessonNumber) {
       case "1":
-        let output = `<span class="cmd">Lesson ${arg}:</span> Find out where you are in the file system with the <em>'pwd'</em> command.`;
-        return output;
+        return `<span class=\"cmd\">Lesson ${lessonNumber}:</span> Find out where you are in the file system with the <em>'pwd'</em> command.`;
       default:
-        console.log(`Sorry`);
+        return `Sorry, lesson ${lessonNumber} not found.`;
     }
   },
 };
